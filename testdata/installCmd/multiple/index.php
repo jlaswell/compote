3a150723f5b36ef8a35f099c27d08e2f5de73ac7
John Laswell <john.n.laswell@gmail.com>
2020-04-08 21:07:11 -0400

    update testdata additions

---
+++ testdata/installCmd/multiple/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Composer\Semver\Semver;
+use Ramsey\Uuid\Uuid;
+
+var_export(Uuid::uuid4());
+
---
+++ testdata/installCmd/single/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Composer\Semver\Semver;
+
+var_export(new Semver());
+
